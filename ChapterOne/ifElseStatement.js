adb4facd446b3eb3a6d37a8e31f7d89589bea03f
mukul <mukul71@gmail.com>
2024-03-24 14:31:25 +0600

    IF Else Statement

---
+++ ChapterOne/ifElseStatement.js
@@ -0,0 +1,11 @@
+var whatDay = "Sunday";
+
+if (whatDay === 'Friday'){
+    console.log("it is closed today");
+} else if(whatDay === "Thursday"){
+    console.log("it is partially closed");
+} else if (whatDay === "Saturday"){
+    console.log("Some office are closed today");
+}else {
+    console.log("It is normal working day")
+}
