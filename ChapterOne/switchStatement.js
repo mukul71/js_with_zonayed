7a4c490097e43ae345c392c63ff5d856c6f4cb54
mukul <mukul71@gmail.com>
2024-03-24 15:27:59 +0600

    First Switch case statement

---
+++ ChapterOne/switchStatement.js
@@ -0,0 +1,29 @@
+var weekDay = 'Sunday';
+
+switch(weekDay){
+    case "Saturday":
+        console.log("Saturday! today is closed");
+        break;
+    
+    case "Sunday":
+        console.log("Sunday! it's normal day");
+        break;
+    case "Monday":
+        console.log("Monday! it's normal day");
+        break;
+    case "Tuesday":
+        console.log("Tuesday! it's normal day");
+        break;
+    case "Wednesday":
+        console.log("Wednesday! it's normal day");
+        break;
+    case "Thursday":
+        console.log("Thursday! it's normal day");
+        break;
+    case "Friday":
+        console.log("Friday! We'r closed today");
+        break;
+    default:
+        console.log("Not a day");
+
+}
