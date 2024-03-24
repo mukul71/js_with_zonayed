c7869bb089ebe0489404aca84c986dcadccef04a
mukul <mukul71@gmail.com>
2024-03-24 15:51:17 +0600

    Second switchCase example

---
+++ ChapterOne/switchTwo.js
@@ -0,0 +1,27 @@
+var rollNo = 6;
+
+switch(rollNo){
+    case 1:
+        console.log("Ramjan");
+        break;
+    case 2:
+        console.log("Shafik");
+        break;
+    case 3:
+        console.log("Tuhin");
+        break;
+    case 4:
+        console.log("Nazmul");
+        break;
+    case 5:
+        console.log("Russel");
+        break;
+    case 6:
+        console.log("Hasan");
+        break;
+    case 7:
+        console.log("Imran");
+        break;
+    default:
+        console.log("None of here");
+}
