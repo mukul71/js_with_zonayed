bfcc41bf8afdf5dd06fc464b8e4e36fe963d9969
mukul <mukul71@gmail.com>
2024-03-25 15:12:06 +0600

    methods added

---
+++ ChapterOne/Array/arrayMethods.js
@@ -0,0 +1,10 @@
+var fruits = ['Banana','Apple','Orange'];
+console.log(fruits);
+//
+//Using methods
+fruits.push('Water melon'); // 'push' adds item at the end
+console.log(fruits);
+fruits.shift();            //'shift' removes the first item
+console.log(fruits);
+fruits.pop();              //'pop' removes the last elemen
+console.log(fruits);
