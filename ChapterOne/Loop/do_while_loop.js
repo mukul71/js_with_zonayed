47a11623c44d2f2f94c9a239d0c5b247bd4cefb6
mukul <mukul71@gmail.com>
2024-03-25 13:43:34 +0600

    loops added

---
+++ ChapterOne/Loop/do_while_loop.js
@@ -0,0 +1,6 @@
+var num = 0; 
+do{
+    console.log('Inside the loop , num is:'+ num);
+    num+= 1;
+}while(num <20);
+console.log("Now outside of the loop");
---
+++ ChapterOne/Loop/while_loop.js
@@ -0,0 +1,6 @@
+var num = 20;
+while(num >10){
+    console.log('Inside the loop'+ num);
+    num =+1;
+}
+console.log("Outside of the loop");
