f15b9c1b41ead1354133fac1018d2077ab739c09
mukul <mukul71@gmail.com>
2024-03-25 15:35:52 +0600

    array methods practice

--- ChapterOne/Array/arrayMethods.js
+++ ChapterOne/Array/arrayMethods.js
@@ -6,5 +6,10 @@ fruits.push('Water melon'); // 'push' adds item at the end
 console.log(fruits);
 fruits.shift();            //'shift' removes the first item
 console.log(fruits);
-fruits.pop();              //'pop' removes the last elemen
+fruits.pop();              //'pop' removes the last element
+console.log(fruits);
+fruits.unshift('Jackfruit'); //unshift adds element at the beginning.
+console.log(fruits);
+console.log (fruits.indexOf('Orange'));
+fruits.splice(1,2);//removes item with index number. here from index 1 and removes 2 items.
 console.log(fruits);
