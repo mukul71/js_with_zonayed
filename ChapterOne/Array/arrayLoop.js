864d2e38e33ebe73802f3523e9e40f7559e2be7f
mukul <mukul71@gmail.com>
2024-03-25 14:19:42 +0600

    Loop used for array

---
+++ ChapterOne/Array/arrayLoop.js
@@ -0,0 +1,5 @@
+var nameArray = ['Jamal','Kamal','Tomal','Heemel','Halim','Dalim'];
+console.log(nameArray);
+for (var i =0;i<nameArray.length; i++){
+    console.log("Names are: "+ nameArray[i]);
+}
---
+++ ChapterOne/Array/nameArray.js
@@ -0,0 +1,7 @@
+var nameArray = ["Rahim","Karim","Rafik","Shafik"];
+console.log(nameArray);
+console.log(nameArray[2]);
+nameArray[3]; // we need to use console.log to see output. 
+console.log(nameArray[3]);
+console.log(nameArray.length); //Dot Notation used
+console.log(nameArray['length']);//Bracket Notation used
