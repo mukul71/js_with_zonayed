7d728f066afe55f61c735cd14590900c0f7aade3
mukul <mukul71@gmail.com>
2024-03-26 15:05:36 +0600

    JS object' first example

---
+++ ChapterOne/Object/object_in_js.js
@@ -0,0 +1,13 @@
+var rahim = {
+    name: "Abdur Rahim",
+    Age: 21,
+    address: "Dhaka", 
+    profession: "Job"
+}
+
+console.log(rahim);
+console.log(rahim.Age);//dot notation
+console.log(rahim.profession);
+console.log(rahim['address']);//braket notation and 
+                              //object property must a 
+                              //string insdie the bracket.
