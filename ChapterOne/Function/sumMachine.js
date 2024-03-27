4bd3322338fa96a03aded04d1282b7ee8c94dc4a
mukul <mukul71@gmail.com>
2024-03-27 15:38:07 +0600

    another function adde

--- ChapterOne/Function/sumMachine.js
+++ ChapterOne/Function/sumMachine.js
@@ -1,4 +1,11 @@
 function sumMachine(a, b){
+    //var sum = a + b; 
+    //return sum; or 
     return a+b;
 }
 console.log(sumMachine(2,2));
+
+function printMyName(name,age){
+    console.log('My name is '+name+' and I am '+age+' years old');
+}
+console.log(printMyName('Mukul',44));
