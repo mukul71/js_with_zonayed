c8f51520a36224aac022b8ae0d491859982a8ada
mukul <mukul71@gmail.com>
2024-03-27 12:16:06 +0600

    Test of first callback function

--- ChapterOne/Object/object_in_js.js
+++ ChapterOne/Object/object_in_js.js
@@ -12,7 +12,11 @@ console.log(rahim['address']);//braket notation and
                               //object property must a 
                               //string insdie the bracket.
 rahim.zipcode= 1222;
-rahim.policeStation = "Kafrul";
+rahim.policeStation = "Kafrul"; // new value added
 
 console.log(rahim.zipcode)//dot notation
-console.log(rahim['policeStation']);
+console.log(rahim['policeStation']);//bracket notation
+
+rahim.job = "Student"; //value changed
+console.log(rahim.job);
+
