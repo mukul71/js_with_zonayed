924ba7770356b5e9f38c4ae23a8db324a5b0275b
mukul <mukul71@gmail.com>
2024-03-27 12:46:47 +0600

    Array added inside object

--- ChapterOne/Object/functionInObject.js
+++ ChapterOne/Object/functionInObject.js
@@ -7,3 +7,17 @@ reaction: function(){
 }
 }
 console.log(fruits);
+console.log(fruits.reaction);
+
+for (item in fruits){
+    console.log(item);
+}
+// Adding array inside Object
+
+var objArr = {
+    normal : "Normal Item",
+    name : ['Rahim','Karim','Rafik','Shafik'],
+    age: [10, 20,30,40]
+}
+
+console.log(objArr.name);
