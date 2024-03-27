2fcdcac29d3589342bb48c184dd902cdc635a8d4
mukul <mukul71@gmail.com>
2024-03-27 14:55:08 +0600

    Function types explained

--- ChapterOne/Function/firstFunc.js
+++ ChapterOne/Function/firstFunc.js
@@ -1,4 +1,19 @@
+//Function Statement
+//When we declare/create a function directly as bellow
+//it is called Function Statement. 
+
 function funcName(){
-    console.log('First Function Declared')
+    console.log('I am function statement style')
 }
-funcName();
+funcName(); 
+
+//Function Expression
+//When we declare a variable and assign an anonymous function
+//to it as its value, it is called Function Expression. 
+//as bellow Exxample: 
+
+var myFunc = function(){
+    console.log(" I am function expresseion style.");
+
+}
+myFunc();
