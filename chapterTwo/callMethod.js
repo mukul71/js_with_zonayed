a008a6fa5a89da3c25a5b214e4d86050294a803d
mukul <mukul71@gmail.com>
2024-03-30 13:34:45 +0600

    Codes for call method

---
+++ chapterTwo/callMethod.js
@@ -0,0 +1,12 @@
+var myCustomObj = {
+    name: 'Zonayed Ahmed',
+    age:21,
+    job: 'Student',
+    anotherObj:{
+        name:'Ahmed Zonayed',
+        value: function(){
+            console.log('My name is ' + this.name);
+        }
+    }
+}
+myCustomObj.anotherObj.value();
