f7ae5e19259eac15d5c0421a95e3253f9036acf7
mukul <mukul71@gmail.com>
2024-03-30 14:10:13 +0600

    .call (myCustomObj) not workin insdie the funciton

--- chapterTwo/callMethod.js
+++ chapterTwo/callMethod.js
@@ -1,3 +1,4 @@
+/*
 var myCustomObj = {
     name: 'Zonayed Ahmed',
     age:21,
@@ -11,3 +12,16 @@ var myCustomObj = {
 }
 myCustomObj.anotherObj.value(); // Output Ahmed Zonayed
 myCustomObj.anotherObj.value.call(myCustomObj); // Output Zonayed Ahmed
+*/
+var myCustomObj = {
+    name: 'Mukul Rahman',
+    age:21,
+    job: 'Student',
+    anotherObj:{
+        name:'Rahman Mukul',
+        value: function(){
+            console.log('My name is ' + this.name);
+        }//.call (myCustomObj) //Not working here
+    }
+}
+myCustomObj.anotherObj.value.call(myCustomObj);
