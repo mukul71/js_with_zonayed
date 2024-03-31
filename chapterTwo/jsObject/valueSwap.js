93a289bc7a121c35fdc6b0425248cfc60d668f82
mukul <mukul71@gmail.com>
2024-03-31 15:08:00 +0600

    New exaple of value swap for Object

--- chapterTwo/jsObject/valueSwap.js
+++ chapterTwo/jsObject/valueSwap.js
@@ -1,3 +1,4 @@
+/*
 var a = 10, b = 20;
 console.log('Before swap: value of a : '+ a +' and value of b: '+b);
 
@@ -10,3 +11,21 @@ function swap(a,b){
 }
 swap(a,b);
 console.log('After swap : value of a : '+ a +' and value of b: '+b);
+*/
+
+var obj = {
+    a : 5,
+    b: 10
+};
+console.log('Befor swap: value of a: '+obj.a+' and value of b; '+obj.b);
+
+function swap(x){
+
+console.log('Befor swap: value of a: '+x.a+' and value of b; '+x.b);
+var temp = x.a;
+x.a = x.b;
+x.b = temp;
+console.log('After swap, inside function: value of a: '+x.a+' and value of b: '+x.b);
+}
+swap(obj);
+console.log('After swap: value of a: '+obj.a+' and value of b; '+obj.b);
