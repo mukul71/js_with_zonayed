a1c4f1a43559fc780159b76a24955c54ffd90b3a
mukul <mukul71@gmail.com>
2024-04-01 12:59:52 +0600

    JS Wrapper

---
+++ chapterTwo/jsObject/jsWrapper.js
@@ -0,0 +1,11 @@
+//var str = 'My name is Zonayed Ahmed';
+//console.dir(str);
+
+var str = new String('My name is Tinsal Chakma');
+console.dir(str);
+
+var num = new Number(10);
+console.dir(num);
+
+var bool = new Boolean(true);
+console.dir(bool);
