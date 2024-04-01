0bc3942746273766c9118bfe1ee932987c48c557
mukul <mukul71@gmail.com>
2024-04-01 14:20:45 +0600

    Function Constructor with propertie

---
+++ chapterTwo/jsObject/constructorNew.js
@@ -0,0 +1,15 @@
+var Person = function(name, age, job){
+    this.name = name;
+    this.age = age;
+    this.job = job;
+}
+var samir = new Person('Samir Hossain',22,'Student');
+var kiran = new Person('Kiran Hossain',29, 'Business');
+var jamil = new Person('Jamils Mia',44, 'Unemployeed');
+var zonayed= new Person('Zonayed Ahmed',28,'Writer');
+
+console.log(samir.name);
+console.log(samir.age);
+console.log(samir.job);
+console.log(kiran.age);
+console.log(zonayed.name);
