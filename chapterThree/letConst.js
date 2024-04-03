335e8231b187d69c093cac927e65f0d608dba701
mukul <mukul71@gmail.com>
2024-04-03 15:05:13 +0600

    let and const used first tiem

---
+++ chapterThree/letConst.js
@@ -0,0 +1,12 @@
+//In 'let' we can re-assign the value of a variable
+
+let name = 'Zonayed Ahmed';
+console.log('My name is '+name);
+name = 'Zawad Ahmed';
+console.log('My name is '+name);
+
+// In 'const' we cannot re-assing the value of a variable 
+
+const b = ' Saiful Islam';
+console.log('My name is '+b);
+b = 'Saifullah Akond'; //Output: TypeError: Assignment to constant variable.
