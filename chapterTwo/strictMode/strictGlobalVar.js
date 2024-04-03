f0017a07b9d50a302c8b3e083e2e6667b74c9269
mukul <mukul71@gmail.com>
2024-04-03 13:26:17 +0600

    use strict used in global variable

---
+++ chapterTwo/strictMode/strictGlobalVar.js
@@ -0,0 +1,14 @@
+/*
+var name = 'Zonayed Ahmed';
+var nama = 'Ahmed Zonayed';
+console .log ('Name:'+name);
+console.log('Mistaken created:'+ nama);
+*/
+//Above code in sloppy mode works fine
+// some code with 'use strict' wont work same
+
+'use strict';
+var name = 'Zonayed Ahmed';
+var nama = 'Ahmed Zonayed';
+console.log('Name: '+name);
+console.log('Mistakenly created:', nama);
