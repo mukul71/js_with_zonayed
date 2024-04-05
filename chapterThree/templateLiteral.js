67a723ee1447d4d7b885f40b32a197c5ae59ffcb
mukul <mukul71@gmail.com>
2024-04-05 16:03:05 +0600

    text format used in template literal

--- chapterThree/templateLiteral.js
+++ chapterThree/templateLiteral.js
@@ -6,7 +6,14 @@ const name = 'Zonayed Ahmed';
 
       console.log(` My name is ${name} and I am ${age} years old. I am a ${work}`);
 
+
+     //function in template literal
       function calculateAge (dob){
         return `I am ${2024 - dob} years old`;
       }
       console.log(calculateAge(1980));
+
+      //text format will be the same in output as writen in code:
+      console.log(`Hello,
+      hour are you today? 
+      I am fine, thank you`);
