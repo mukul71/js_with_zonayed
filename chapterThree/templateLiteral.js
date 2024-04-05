adecb0e8835ddff9a743e38591c9327416548ff3
mukul <mukul71@gmail.com>
2024-04-05 15:42:43 +0600

    Template literal used

---
+++ chapterThree/templateLiteral.js
@@ -0,0 +1,7 @@
+// `` backtic sign is used to reduce code complexity (started from ES6) 
+
+const name = 'Zonayed Ahmed';
+      age = 22;
+      work = 'Student';
+
+      console.log(` My name is ${name} and I am ${age} years old. I am a ${work}`);
