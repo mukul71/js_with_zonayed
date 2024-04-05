84d799c0bbe588ea741c5076d0d1ab1a179a77c3
mukul <mukul71@gmail.com>
2024-04-05 15:58:38 +0600

    funciton used in template literal

--- chapterThree/templateLiteral.js
+++ chapterThree/templateLiteral.js
@@ -5,3 +5,8 @@ const name = 'Zonayed Ahmed';
       work = 'Student';
 
       console.log(` My name is ${name} and I am ${age} years old. I am a ${work}`);
+
+      function calculateAge (dob){
+        return `I am ${2024 - dob} years old`;
+      }
+      console.log(calculateAge(1980));
