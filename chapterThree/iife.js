9d375fe4db6ed1287edadc5027ef8bacaf1742f0
mukul <mukul71@gmail.com>
2024-04-05 15:00:45 +0600

    example added

--- chapterThree/iife.js
+++ chapterThree/iife.js
@@ -1,4 +1,16 @@
 (function aDemoFunc(){
     var msg = 'Hello World';
     console.log(msg);
-})();
+})(); //output: Hello World
+
+//From ES6, we can use let or const to get iife facility 
+
+{
+    let a = 5;
+    let b = 10;
+    const pi = 3.1416;
+    console.log(a+b+pi);// output 18.1416
+}
+// we cannot access above variables from outside of this scope
+
+console.log('value of pi is:'+pi); // pi is not defined
