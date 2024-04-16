7bcdb96c2ee77dc8658c8303f9b7dda077ebb5cb
mukul <mukul71@gmail.com>
2024-04-16 14:49:19 +0600

    startsWith used

---
+++ chapterThree/stringMethod.js
@@ -0,0 +1,5 @@
+//startsWith: specify if a string starts with a specific character/characterset
+const start = 'My name is Khan';
+console.log(start.startsWith('My'));
+console.log(start.startsWith('Khan'));
+console.log(start.startsWith('M'));
