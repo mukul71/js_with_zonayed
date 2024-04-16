0f54e6daccd6e932061bf99d4e4588744baa35aa
mukul <mukul71@gmail.com>
2024-04-16 14:59:15 +0600

    includes method used

--- chapterThree/stringMethod.js
+++ chapterThree/stringMethod.js
@@ -1,5 +1,17 @@
 //startsWith: specify if a string starts with a specific character/characterset
+console.log('startsWith Method:');
 const start = 'My name is Khan';
 console.log(start.startsWith('My'));
 console.log(start.startsWith('Khan'));
 console.log(start.startsWith('M'));
+
+//endsWith: specify if a strings ends with a specific character/characterset 
+console.log('endsWith Method:')
+const end = ' I love programming';
+console.log(end.endsWith('programming'));
+
+//includes: specify if a string contains a specific character/characterset 
+
+console.log('includes Method');
+const inc = 'I love programming with JavaScript';
+console.log(inc.includes('JavaScript'));
