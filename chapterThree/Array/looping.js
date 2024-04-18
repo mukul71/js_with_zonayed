78fc90890965c3df35e0641623527a8699d59c1b
mukul <mukul71@gmail.com>
2024-04-18 16:08:04 +0600

    finding index in ES6

--- chapterThree/Array/looping.js
+++ chapterThree/Array/looping.js
@@ -3,3 +3,11 @@ for (const number of numbers){
     if(number === 5) break;
     console.log(`Number printed ${number}`);
 }
+
+//Finding index number in ES6 
+console.log();
+console.log('Finding index number');
+var numbers = [19,23,14,56,32,65,10,45];
+var indx = numbers.findIndex(number =>number ===65);
+console.log(numbers);
+console.log(`Index of 64 is ${indx}`);
