727da47b8d9461248d33449a362388ec00936021
mukul <mukul71@gmail.com>
2024-04-18 16:47:07 +0600

    spread operator used in array methods

--- chapterThree/spreadOperator.js
+++ chapterThree/spreadOperator.js
@@ -10,3 +10,25 @@ calculate(numbers); //Output 1,2,3,4 undefined, undefined, undefined as
 console.log();
 console.log('spread operator ... used');
 calculate(... numbers); // Output Sum:10 
+
+//spread operator in array methods
+console.log();
+console.log('spread operator in array method');
+
+//ES5
+var arrName = ['Rahim','karim','Rafik','Jabbar'];
+console.log(arrName);
+//Adding one element at beginning and ending
+
+arrName.unshift('Shfik');
+arrName.push('Salam');
+console.log(arrName);
+
+// ES6 with spread operator
+console.log();
+const arrName6 = ['Bangladesh', ...arrName]; //for array.unshiift()
+console.log(arrName6);
+const arrName7 = [...arrName6,'Language'];
+console.log(arrName7);
+
+
