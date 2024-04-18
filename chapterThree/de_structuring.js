384ca49e302da3c048bbae147f358561486777b1
mukul <mukul71@gmail.com>
2024-04-18 14:22:31 +0600

    Destructuring in ES5

---
+++ chapterThree/de_structuring.js
@@ -0,0 +1,9 @@
+//with ES5 
+const zonayed = ['Zonayed Ahmed',21, 'Student' ];
+var name = zonayed[0];
+var age = zonayed[1];
+var profession = zonayed[2];
+
+console.log(name);
+console.log(age);
+console.log(profession);
