ae1e36052c5e7e6276bc1b0cef620d42332c30f4
mukul <mukul71@gmail.com>
2024-04-18 14:27:03 +0600

    Destructuring in ES6

--- chapterThree/de_structuring.js
+++ chapterThree/de_structuring.js
@@ -1,9 +1,18 @@
 //with ES5 
+console.log('Destructuring in ES5')
 const zonayed = ['Zonayed Ahmed',21, 'Student' ];
 var name = zonayed[0];
 var age = zonayed[1];
 var profession = zonayed[2];
 
+console.log(name);
+console.log(age);
+console.log(profession);
+
+//with ES6
+console.log();
+console.log('Destructuring in ES6');
+const [name6, age6, profession6] = zonayed;
 console.log(name);
 console.log(age);
 console.log(profession);
