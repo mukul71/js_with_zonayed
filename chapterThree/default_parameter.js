c07675c16915d44d42be2425bfb16924963e7eb4
mukul <mukul71@gmail.com>
2024-04-19 14:44:45 +0600

    default parameter used

--- chapterThree/default_parameter.js
+++ chapterThree/default_parameter.js
@@ -5,3 +5,18 @@ function defaultParameter6 (name= 'Zonayed Ahmed', age=44){
 }
 defaultParameter6();
 defaultParameter6('Saiful',33);
+
+//Default parameter in function
+console.log();
+function FunctionConst6 (name = 'Zonayed Ahmed', age = 22){
+    this.name = name;
+    this.age =age;
+}
+const zonayed = new FunctionConst6()
+console.log(zonayed.name);
+console.log(zonayed.age);
+
+console.log();
+const zonayed2 = new FunctionConst6('Ahmed Zonayed', 25);
+console.log(zonayed2.name);
+console.log(zonayed2.age);
