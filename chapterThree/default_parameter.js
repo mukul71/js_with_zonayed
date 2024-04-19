415d301f061d96c9f4ccd0cd4742dc3123b680a6
mukul <mukul71@gmail.com>
2024-04-19 14:23:20 +0600

    Default parameter used

---
+++ chapterThree/default_parameter.js
@@ -0,0 +1,7 @@
+//default parameter is used in case if we do not pass any argument to a function when we call it
+
+function defaultParameter6 (name= 'Zonayed Ahmed', age=44){
+    console.log(`My name is: ${name} and I am ${age} years old`);
+}
+defaultParameter6();
+defaultParameter6('Saiful',33);
