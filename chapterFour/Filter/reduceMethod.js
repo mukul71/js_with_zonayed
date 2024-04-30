4ea03d30e313d4db52f9e319816614306d129d2e
mukul <mukul71@gmail.com>
2024-04-30 14:42:59 +0600

    initial value explained

--- chapterFour/Filter/reduceMethod.js
+++ chapterFour/Filter/reduceMethod.js
@@ -1,5 +1,5 @@
 var arr = [1,2,3,4,5,6,7,8,9];
 var total = arr.reduce(function(sum,item){
     return sum += item;
-},0);
+},0); //0 is the initial value, it can be any value to start with
 console.log(total);
