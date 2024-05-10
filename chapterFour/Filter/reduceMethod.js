dc323b0be81db3a008cf961ae6ca369652296ac5
mukul <mukul71@gmail.com>
2024-05-10 12:44:29 +0600

    basic loop operation in object

--- chapterFour/Filter/reduceMethod.js
+++ chapterFour/Filter/reduceMethod.js
@@ -3,3 +3,9 @@ var total = arr.reduce(function(sum,item){
     return sum += item;
 },0); //0 is the initial value, it can be any value to start with
 console.log(total);
+
+console.log();
+console.log('Reduce used in ES6');
+
+const t = arr.reduce((sum,item)=> sum += item, 2);
+console.log(t);
---
+++ chapterFour/loopsInObj.js
@@ -0,0 +1,13 @@
+var obj = {
+    Bangladesh : 'Dhaka',
+    India : 'Delhi',
+    Pakistan : 'Islamabad',
+    Nepal : 'Kathmandu',
+    Sri_Lanka : 'Colombo',
+    Thailand : 'Bangkok',
+    Afganistan : 'Kabul'
+}
+
+for(name in obj){
+    console.log(name);
+}
